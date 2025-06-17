b1b176af6e8b247a89a338cbce237280f0e82348
sanjuka12 <dishansanjuka3@gmail.com>
2025-06-17 20:43:51 +0530

    changed backend new

--- Config/firebaseConfig.js
+++ Config/firebaseConfig.js
@@ -9,3 +9,26 @@ admin.initializeApp({
 });
 
 module.exports = admin.firestore();
+
+
+/*// config/firebaseConfig.js
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+// Check if database is accessible
+(async () => {
+  try {
+    await db.collection("Login").limit(1).get(); // simple test query
+    console.log("✅ Firestore DB Connected");
+  } catch (error) {
+    console.error("❌ Firestore DB Disconnected", error.message);
+  }
+})();
+
+module.exports = db;*/
