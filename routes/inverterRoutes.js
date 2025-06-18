457c86394da2def53a41ac8d425079dd6e0a02b4
sanjuka12 <dishansanjuka3@gmail.com>
2025-06-19 01:01:04 +0530

    inverter routes

--- routes/inverterRoutes.js
+++ routes/inverterRoutes.js
@@ -1,19 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-// const { fetchInverterData, insertInverterData } = require('../controllers/inverterController');
-
-// router.get('/inverter-data', fetchInverterData); //getting solar inverter data
-// router.post('/inverter-data', insertInverterData); //Adding Solar inverter data
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const { fetchInverterData, insertInverterData } = require('../controllers/inverterController');
-const checkAuth = require('../middleware/authMiddleware'); // 👈 import middleware
 
-// 👇 protect both GET and POST routes
-router.get('/inverter-data', checkAuth, fetchInverterData); 
-router.post('/inverter-data', checkAuth, insertInverterData); 
+ router.get('/inverter-data', fetchInverterData); //getting solar inverter data
+ router.post('/inverter-data', insertInverterData); //Adding Solar inverter data
 
-module.exports = router;
+ module.exports = router;
+
+
