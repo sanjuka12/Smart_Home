f58171de1f946acd1ab017092c40d3413c94842c
sanjuka12 <dishansanjuka3@gmail.com>
2025-06-19 00:21:58 +0530

    Added authMiddleware.js

--- routes/inverterRoutes.js
+++ routes/inverterRoutes.js
@@ -1,8 +1,19 @@
+// const express = require('express');
+// const router = express.Router();
+// const { fetchInverterData, insertInverterData } = require('../controllers/inverterController');
+
+// router.get('/inverter-data', fetchInverterData); //getting solar inverter data
+// router.post('/inverter-data', insertInverterData); //Adding Solar inverter data
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const { fetchInverterData, insertInverterData } = require('../controllers/inverterController');
+const checkAuth = require('../middleware/authMiddleware'); // 👈 import middleware
 
-router.get('/inverter-data', fetchInverterData); //getting solar inverter data
-router.post('/inverter-data', insertInverterData); //Adding Solar inverter data
+// 👇 protect both GET and POST routes
+router.get('/inverter-data', checkAuth, fetchInverterData); 
+router.post('/inverter-data', checkAuth, insertInverterData); 
 
 module.exports = router;
